0a3ebac8a1b4f7e7ecbee35da4ebd9e757d81959
clarke <23103439@usc.edu.ph>
2025-09-13 15:50:45 +0800

    add customer ccontroller, add customer repositry function

---
+++ backend/SalesSystem/Controllers/CustomerController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SalesSystem.Data;
+using SalesSystem.Interfaces;
+using SalesSystem.Repositries;
+namespace SalesSystem.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+        private readonly DatabaseContext _context;
+        private readonly ICustomerRepository _customerRepository;
+
+        public AnimalController(DatabaseContext context, ICustomerRepository customerRepository)
+        {
+            _context = context;
+            _customerRepository = customerRepository;
+        }
+
+        [HttpGet]
+        [Route("getAllCustomers")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> GetAllCustomers()
+        {
+            var customers = await _customerRepository.GetAllCustomers();
+            return Ok(customers);
+        }
+
+        //[HttpGet("getcustomer/{id}")]
+        //[ValidateAntiForgeryToken]
+
+        
+    }
+}
--- backend/SalesSystem/Repositries/CustomerRepository.cs
+++ backend/SalesSystem/Repositries/CustomerRepository.cs
@@ -1,6 +1,68 @@
-﻿namespace SalesSystem.Repositries
+﻿using SalesSystem.Interfaces;
+using SalesSystem.Data;
+using SalesSystem.Models;
+using Microsoft.EntityFrameworkCore;
+namespace SalesSystem.Repositries
 {
-    public class CustomerRepository
+    public class CustomerRepository : ICustomerRepository
     {
+        private readonly DatabaseContext _context;
+
+        public CustomerRepository(DatabaseContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Customer>> GetAllCustomers()
+        {
+            return await _context.customers.ToListAsync();
+        }
+
+        public async Task<Customer> GetCustomerByIdAsync(int id)
+        {
+            var customer = await _context.customers.FindAsync(id);
+            if (customer == null)
+            {
+                throw new InvalidOperationException($"Customer with ID {id} not found.");
+            }
+            return customer;
+        }
+
+        public async Task<Customer> AddCustomerAsync(Customer customer)
+        {
+            await _context.customers.AddAsync(customer);
+            await _context.SaveChangesAsync();
+            return customer;
+        }
+
+        public async Task<Customer> UpdateCustomerAsync(Customer customer)
+        {
+            var existingCustomer = await _context.customers.FirstOrDefaultAsync(c => c.CustomerId == customer.CustomerId);
+            if (existingCustomer == null)
+            {
+                throw new InvalidOperationException($"Customer with ID {customer.CustomerId} not found.");
+            }
+            else
+            {
+
+                existingCustomer.Name = customer.Name;
+                existingCustomer.Contact_No = customer.Contact_No;
+            }
+            await _context.SaveChangesAsync();
+            return existingCustomer;
+        }
+
+        public async Task<bool> DeleteCustomerAsync(int id)
+        {
+            var customer = await _context.customers.FindAsync(id);
+            if (customer == null)
+            {
+                return false;
+            }
+
+            _context.customers.Remove(customer);
+            await _context.SaveChangesAsync();
+            return true;
+        }
     }
 }
